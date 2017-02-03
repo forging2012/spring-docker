727bfbffb201ba548cfb43bc1ecde501cdba9814
bin <445488118@qq.com>
2017-02-03 10:37:04 +0800

    first commit

---
+++ src/main/java/com/becool/demo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.becool.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+	@RequestMapping(value="/")
+	public String index(){
+		return "index";
+	}
+}
